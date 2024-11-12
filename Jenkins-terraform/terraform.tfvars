59358439dbf39f8a850ba0d22b191cc4a0f258a0
Pavan M <132834477+Pavangowda73@users.noreply.github.com>
2024-11-12 18:24:42 +0530

    Add files via upload

---
+++ Jenkins-terraform/terraform.tfvars
@@ -0,0 +1,3 @@
+ami_value = "ami-09b0a86a2c84101e1"
+instancetype = "t2.large"
+ssh = "pavan"
---
+++ Jenkins-terraform/variable.tf
@@ -0,0 +1,11 @@
+variable "ami_value" {
+  description = "ami for instance"
+}
+
+variable "instancetype" {
+  description = "instance type"
+}
+
+variable "ssh" {
+  description = "ssh for ec2"
+}
